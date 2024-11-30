320a31a6411b9f9c4160e9d62a292a7e23067c60
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2024-11-30 16:28:24 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, StyleSheet } from "react-native";
+
+export default function App() {
+  return <SafeAreaView style={styles.container}></SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
